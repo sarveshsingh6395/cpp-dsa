24cf75ba4cb0fb1bbd1263d3d191eb23ea789342
Sarvesh singh <sarveshsingh9381@gmail.com>
2024-02-24 15:40:18 +0530

    initial

--- p.cpp
+++ p.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main () {
 	int n;
 	cin>>n;
-	
+	// pattern
 	int i=1;
 	while(i<=n) {
 		int j=1;
--- suman.cpp
+++
@@ -1,5 +0,0 @@
-#include<iostream>
-using namespace std;
-int main() {
-	cout<<"Suman is pagal"<<endl;
-}
