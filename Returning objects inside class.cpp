baef40e4e6a49a39ab7a53610531d6da6ca9a1a0
Sarvesh singh <sarveshsingh9381@gmail.com>
2024-03-02 17:11:43 +0530

    returning objects inside class

---
+++ Returning objects inside class.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+class matrix{
+	int item[3][3];
+	public:
+		void getdata(void);
+		void putdata(void);
+		matrix add(matrix x, matrix y)
+		{
+			matrix h;
+			for(int i=0;i<3;i++)
+ 		   {
+ 			   	for(int j=0; j<3;j++)
+  			  	{
+ 			   		h.item[i][j] = x.item[i][j] + y.item[i][j];
+				}
+			}
+			return (h);
+		}
+};
+void matrix :: getdata(void)
+{
+	for(int i=0;i<3;i++)
+	{
+		for(int j=0;j<3;j++)
+		{
+			cin>>item[i][j];
+		}
+	}
+}
+
+void matrix :: putdata(void)
+{
+	for(int i=0;i<3;i++)
+	{
+		for(int j=0;j<3;j++)
+		{
+			cout<<item[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+}
+int main() 
+{
+	matrix a,b,c,result;
+	cout<<"Enter matrix A : "<<endl;
+	a.getdata();
+	cout<<"Enter matrix B : "<<endl;
+	b.getdata();
+	cout<<"You entered matrix A : "<<endl;
+	a.putdata();
+	cout<<"You entered matrix B : "<<endl;
+	b.putdata();
+	result=c.add(a,b);
+	cout<<"Addition :"<<endl;
+	result.putdata();
+	return 0;
+}
