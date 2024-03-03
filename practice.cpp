f5adf366c7c2be84145914fc7b97333c3bc034d4
Sarvesh singh <sarveshsingh9381@gmail.com>
2024-03-03 23:30:13 +0530

    practice

--- practice.cpp
+++ practice.cpp
@@ -1,58 +1,22 @@
 #include<iostream>
 using namespace std;
-class matrix{
-	int item[3][3];
-	public:
-	void getdata(void);
-	void putdata(void);
-	void product(matrix, matrix);
+class sum{
+	int a,b;
+		friend int add(void);
 };
-void matrix :: getdata(void)
+int add(void)
 {
-	for(int i=0;i<3;i++)
-	{
-		for(int j=0;j<3;j++)
-		{
-			cin>>item[i][j];
-		}
-	}
-}
-void matrix :: putdata(void)
-{
-	for(int i=0;i<3;i++)
-	{
-		for(int j=0;j<3;j++)
-		{
-			cout<< item[i][j]<<"  ";
-		}
-		cout<<endl;
-	}
-}
-void matrix :: product(matrix x, matrix y)
-{
-	for(int i=0; i<3 ; i++)
-	{
-		for(int j=0; j<3;j++)
-		{
-			item[i][j] = x.item[i][j] * y.item[i][j];
-		}
-	}
+	sum s;
+	int num;
+	s.a =9;
+	s.b =8;
+	num = s.a + s.b;
+	return num;
 }
 int main()
 {
-	matrix a,b,result;
-	cout<<"Enter Matrix a: "<< endl;
-	a.getdata();
-	cout<<"Enter Matrix b: "<< endl; 
-	b.getdata();
-	cout<<"You entered matrix A : "<< endl;
-	a.putdata();
-	cout<<"You entered matrix B : "<< endl;
-	b.putdata();
-	result.product(a,b);
-	cout<<"Product : " << endl;
-	result.putdata();
+	int result;
+	result = add();
+	cout<<"Additon : "<<" "<<result<<endl;
+	return 0;
 }
-
-
-
